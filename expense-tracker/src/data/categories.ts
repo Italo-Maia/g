404f69c44ed27f9a974c81cae0c6c20f34e7c28d
Italo Maia <italomaiatech@gmail.com>
2022-02-23 19:26:56 -0300

    Sistema Financeiro com react

---
+++ expense-tracker/src/data/categories.ts
@@ -0,0 +1,7 @@
+import {Category} from '../types/Category'
+
+export const categories: Category = {
+    food: {title: 'Alimentação', color: 'blue', expense: true},
+    rent: {title: 'Aluguel', color: 'blue', expense: true},
+    salary: {title: 'Salário', color: 'blue', expense: false}
+}
---
+++ expense-tracker/src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2022, 2, 23), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 2, 23), category: 'food', title: 'Burger King', value: 30 },
+    { date: new Date(2022, 2, 23), category: 'rent', title: 'Aluguel', value: 32.12 },
+    { date: new Date(2022, 2, 23), category: 'salary', title: 'Salário Mensal', value: 32.12 },
+];
